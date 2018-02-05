51b29fd409988dc7401d081345bb315e48a997ef
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2018-02-05 06:16:06 -0500

    app is broken .... -a

--- src/App.js
+++ src/App.js
@@ -4,13 +4,55 @@ import './App.css';
 
 let defaultStyle = {
   color: '#fff',
-}
+};
+let fakeServerData = {
+  user: {
+    name: 'Rambo',
+    playlists: [
+      {
+        name: 'My Favorites',
+        songs: [
+          {name:'Beat it', duration: 1234},
+          {name: 'Hello', duration: 1234}
+        ]
+      },
+      {
+        name: 'My Favorites',
+        songs: [
+          {name:'Beat it', duration: 1234},
+          {name: 'Hello', duration: 1234}
+        ]
+      },
+      {
+        name: 'My Favorites',
+        songs: [
+          {name:'Beat it', duration: 1234},
+          {name: 'Hello', duration: 1234}
+        ]
+      }
+    ]
+  }
+};
 
-class Aggregate extends Component{
+class PlaylistCounter extends Component{
   render(){
     return(
       <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
-        <h2>Number Text</h2>
+        <h2>{this.props.playlists.length} playlists</h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component{
+  render(){
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs)
+    },[])
+    // let totalDuration =
+    return(
+      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+        <h2>{allSongs.length} hours</h2>
       </div>
     );
   }
@@ -40,20 +82,39 @@ class Playlist extends Component{
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {serverData: {}};
+  }
+  componentDidMount() {
+    /*
+    setTimeout( () => {
+      this.setState({ serverData: fakeServerData });
+    }, 1000);
+    */
+    this.setState({ serverData: fakeServerData });
+  }
   render() {
-    let name = 'Ram';
-    let headerStyle = {color: 'lightgreen', 'font-size': '50px'};
-
+    // let name = 'Ram';
+    // let headerStyle = {color: 'lightgreen', 'font-size': '50px'};
     return (
       <div className="App">
-        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
-      <Aggregate />
-      <Aggregate />
-      <Filter />
-      <Playlist />
-      <Playlist />
-      <Playlist />
-      <Playlist />
+        {this.state.serverData.user ?
+        <div>
+          {this.state.serverData.user &&
+          <h1 style={{...defaultStyle, 'font-size': '54px'}}>
+            {this.state.serverData.user.name}'s Playlist
+          </h1>}
+            console.log('my state: ' this.state);
+            <PlaylistCounter playlist={this.state.serverData.user.playlists}/>
+            <HoursCounter playlist={this.state.serverData.user.playlists}/>
+          <Filter />
+          <Playlist />
+          <Playlist />
+          <Playlist />
+          <Playlist />
+        </div> : <h1 style={defaultStyle}>'Loading...'</h1>
+        }
       </div>
     );
   }
