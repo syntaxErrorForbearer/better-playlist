bcb4c05f41aa5ceb8b688b2f608dfdca022d24e2
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2018-02-18 07:21:12 -0500

    Added updating time options and dropdown that updates options array within state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 // vim: syntax=JSX
 import React, { Component } from 'react';
+import 'reset-css/reset.css';
 import logo from './logo.svg';
 import './App.css';
 
+
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
 import queryString from 'query-string';
+
+// let options = ['Milliseconds', 'Seconds', 'Minutes', 'Hours']
 
 let defaultStyle = {
   color: '#fff',
-};
-let fakeServerData = {
-  user: {
-    name: 'Rambo',
-    playlists: [
-      {
-        name: 'Favorites',
-        songs: [
-          {name:'Beat it', duration: 1234},
-          {name: 'Hello', duration: 12.96}
-        ]
-      },
-    ]
-  }
-};
+  fontFamily: 'FreeMono'
+}
+let counterStyle = {...defaultStyle,
+  width: "40%",
+  display: 'inline-block',
+  marginBottom: '10px',
+  fontSize: '14px',
+  lineHeight: '30px',
+}
+
+// let dropdownPlaceholder = document.getElementsByClassName('Dropdown-placeholder')
+// dropdownPlaceholder.style({backgroundColor: 'blue'})
 
 class PlaylistCounter extends Component{
   render(){
+    let playlistCounterStyle = counterStyle
     return(
-      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+      <div style={playlistCounterStyle}>
         <h2>{this.props.playlists.length} playlists</h2>
       </div>
     );
@@ -35,19 +39,56 @@ class PlaylistCounter extends Component{
 
 class HoursCounter extends Component{
   render(){
+
     let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
       return songs.concat(eachPlaylist.songs)
     },[])
-    let totalDuration = allSongs.reduce((sum, eachSong) => {
-      return sum + eachSong.duration
+    let totalDuration = allSongs.reduce((accumulator, currentSong ) => {
+      return accumulator + currentSong.duration
     }, 0)
+
+    console.log('myOption: ', this.props.option);
+    let selectedTime = !this.props.option? 'Hours' : this.props.option
+
+      console.log('selectedTime: ', selectedTime);
+
+    let parsedDuration = (totalDuration, selectedTime) => {
+
+      console.log('inside pd totalDuration: ', totalDuration);
+      console.log('inside pd selectedTime: ', selectedTime);
+
+      let totalDurationMSec = Math.round(totalDuration)
+      let totalDurationSec = totalDurationMSec / 1000
+      let totalDurationMin = totalDurationSec / 60
+      let totalDurationHour = totalDurationMin / 60
+
+      let returnedDuration = selectedTime === 'Seconds' ? totalDurationSec
+         : selectedTime === 'Minutes' ? totalDurationMin
+         : selectedTime === 'Hours' ? totalDurationHour
+         :                   totalDurationMSec
+
+         console.log('inside returnedDuration: ', returnedDuration);
+         return returnedDuration
+
+    }
+
+    console.log('pd: ', parsedDuration(totalDuration, selectedTime));
+    let isTooLow = totalDuration < 1080000
+    let hoursCounterStyle = {...counterStyle,
+        color: isTooLow ? 'red' : 'green',
+        fontWeight: isTooLow ? 'bold' : 'normal',
+    }
+
     return(
-      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
-       {/*<h2>{Math.round(totalDuration)} hours</h2>*/}
-        <h2>
-          {parseFloat(totalDuration / (1000 * 60 * 60))
-              .toFixed(2)} hours
-        </h2>
+      <div>
+        <div style={hoursCounterStyle}>
+        <h1>{parsedDuration(totalDuration, selectedTime)}</h1>
+         {/*<h2>{Math.round(totalDuration)} hours</h2>
+          <h2>
+            {parseFloat(totalDuration / (1000 * 60 * 60))
+                .toFixed(2)} hours
+          </h2>*/}
+        </div>
       </div>
     );
   }
@@ -86,7 +127,11 @@ class App extends Component {
     super();
     this.state = {
       serverData: {},
-      filterString: ''
+      filterString: '',
+      selected: {
+        label: 'Hours'
+      },
+      options : ['Milliseconds', 'Seconds', 'Minutes', 'Hours'],
     }
   }
   componentDidMount() {
@@ -107,9 +152,7 @@ class App extends Component {
       headers: {'Authorization': 'Bearer ' + accessToken}
     }).then(response => response.json())
     .then(playlistData => {
-      console.log("playlistData: ", playlistData)
       let playlists = playlistData.items
-      console.log("playlists: ", playlists)
       let trackDataPromises = playlists.map(playlist => {
         let responsePromise = fetch(playlist.tracks.href, {
           headers: {'Authorization': 'Bearer ' + accessToken}
@@ -121,7 +164,6 @@ class App extends Component {
       let allTracksDataPromises =
         Promise.all(trackDataPromises)
       let playlistsPromise = allTracksDataPromises.then(trackDatas => {
-        console.log("trackDatas: ", trackDatas);
         trackDatas.forEach((trackData, i) => {
           playlists[i].trackDatas = trackData.items
           .map(item => item.track)
@@ -130,38 +172,41 @@ class App extends Component {
             duration: trackData.duration_ms
           }))
         })
-        console.log("after op playlists: ", playlists)
         return playlists
       })
-      console.log('playlistsPromise:', playlistsPromise);
+      // console.log('playlistsPromise:', playlistsPromise);
       return playlistsPromise
     })
     .then(playlists => this.setState({
       playlists: playlists.map(item => {
-        console.log('YOUR DATA: ', item.trackDatas)
+        // console.log('YOUR DATA: ', item.trackDatas)
         return {
           name: item.name,
           imageUrl: item.images[0].url,
-          songs: item.trackDatas.slice(0,3)        }
+          songs: item.trackDatas.slice(0,5)
+        }
       })
     }))
 
   }
   render() {
+
     let playlistToRender =
      this.state.user &&
      this.state.playlists
       ? this.state.playlists.filter(playlist => {
           let matchesPlaylist = playlist.name.toLowerCase().includes(
             this.state.filterString.toLowerCase())
-            console.log('matchesPlaylist: ', matchesPlaylist)
+            // console.log('matchesPlaylist: ', matchesPlaylist)
           let matchesSong = playlist.songs.find(song => song.name.toLowerCase()
             .includes(this.state.filterString.toLowerCase()))
-            console.log('matchesSong: ', matchesSong);
+            // console.log('matchesSong: ', matchesSong);
             return matchesPlaylist || matchesSong
 
       }) : []
 
+      let reduceOptions =
+        () => this.state.options.filter(o => o !== this.state.selected.label)
 
     return (
       <div className="App">
@@ -171,8 +216,14 @@ class App extends Component {
               {this.state.user.name}'s Playlist
             </h1>
               <PlaylistCounter playlists={playlistToRender} />
-              <HoursCounter playlists={playlistToRender} />
-              <Filter onTextChange={text => {
+              <HoursCounter  playlists={playlistToRender} option={this.state.selected.label}/>
+              <div className="dropdown-div">
+                <Dropdown className="dropdown-class" options={reduceOptions()}
+                  onChange={e => this.setState({selected: {label: e.label}})}
+                  value={this.state.selected.label} placeholder="Select an option"
+                />
+              </div>
+              <Filter style={{margin: '5px'}} onTextChange={text => {
                 this.setState({filterString: text})
               }}/>
               {playlistToRender.map(playlist =>
